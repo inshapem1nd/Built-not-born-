3db258ecb48874db0f36011f325e7d2c214369f9
inshapem1nd <inshapemind@outlook.com>
2026-06-15 10:50:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Blueprint from './Blueprint.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Blueprint />
+  </React.StrictMode>,
+)
